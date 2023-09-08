b609f250d870af57274f91751835149bd310902e
saif <saifrafat99@gmail.com>
2023-09-08 06:21:44 +0300

    first commit

---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,51 @@
+import {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+import './Header.scss'
+import {MdFoodBank} from 'react-icons/md'
+import {IoMdMenu} from 'react-icons/io'
+import { useSidebarContext } from '../../context/sidebarContext'
+
+const Navbar = () => {
+  const {openSidebar} = useSidebarContext()
+  const [scrolled, setScrolled] = useState(false)
+
+  
+  const handleScroll = () => {
+    const offset = window.scrollY
+    if (offset > 70) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar bg-orange flex align-center ${scrolled ? 'scrolled' : 'scrolled'}`}>
+        <div className='container w-100'>
+          <div className='navbar-content text-white'>
+              <div className='brand-and-toggler flex align-center justify-between'>
+                <Link to='/' className='navbar-brand fw-3 fs-22 flex align-center'>
+                  <MdFoodBank/>
+                  <span className='navbar-brand-text fw-7'>FastFood.</span>
+                </Link>
+                <div className='navbar-btns flex align-center'>
+                  <button type='button' className='navbar-show-btn text-white' onClick={() => openSidebar()}>
+                      <IoMdMenu size={27}/>
+                  </button>
+                </div>
+              </div>
+          </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Sidebar.scss'
+import { useSidebarContext } from '../../context/sidebarContext'
+import { Link } from 'react-router-dom'
+import { ImCancelCircle } from 'react-icons/im'
+import {useMealContext} from '../../context/mealContext'
+
+const Sidebar = () => {
+  const {isSidebarOpen, closeSidebar} = useSidebarContext()
+  const {categories} = useMealContext()
+
+  return (
+      <nav className={`sidebar ${isSidebarOpen ? 'sidebar-visible' : ''}`}>
+          <button type='button' className='navbar-hide-btn' onClick={() => closeSidebar()}>
+             <ImCancelCircle size={24}/>
+          </button>
+          <div className='side-content'>
+            <ul className='side-nav'>
+              {categories.map(category => (
+              <li className='side-item' key={category.idCategory}>
+                <Link to={`/meal/category/${category.strCategory}`} className='side-link ls-1 fs-13' onClick={() => closeSidebar()}>
+                  {category.strCategory}
+                </Link>
+              </li>
+              ))}
+            </ul>
+          </div>
+      </nav>
+  )
+}
+
+export default Sidebar
